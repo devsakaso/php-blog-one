998c12bd4d844ccbe75ecb684b4990252abf5511
devsakaso <pg165m@gmail.com>
2021-07-30 03:50:54 +0900

    Create header and footer of admin and removed unnecessary codes

---
+++ admin/sidebar.php
@@ -0,0 +1,22 @@
+<nav class="navbar-default navbar-side" role="navigation">
+    <div class="sidebar-collapse">
+        <ul class="nav" id="main-menu">
+
+            <li>
+                <a class="active-menu" href="index.php"><i class="fa fa-dashboard"></i> ダッシュボード</a>
+            </li>
+            <li>
+                <a href="blog-category.php"><i class="fa fa-desktop"></i> ブログカテゴリー</a>
+            </li>
+             <li>
+                <a href="blogs.php"><i class="fa fa-bar-chart-o"></i> ブログ</a>
+            </li>
+            <li>
+                <a href="write-a-blog.php"><i class="fa fa-qrcode"></i> ブログ投稿</a>
+            </li>
+        </ul>
+
+    </div>
+
+</nav>
+<!-- /. NAV SIDE  -->
